f3506882b890c9b9c7eb7dab98adeed129e65409
jeffrey <jeffondev@gmail.com>
2021-04-03 18:14:49 +0900

    nodejs 라우팅

--- nodejs/myapp/app.js
+++ nodejs/myapp/app.js
@@ -5,6 +5,14 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/jinho', function (req, res) {
+  console.log('Accessing the secret section ...');
+  res.send('Hello jinho!')
+})
+
+app.post('/jinho', (req, res) => {
+  res.send('post Hello jinho!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
